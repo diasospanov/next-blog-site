c4f2ef49092f8be3cabffe2582eee888ed24c64b
Di <dias.ospanov@gmail.com>
2023-05-05 18:22:39 +0300

    Add blog form.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,30 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
+import Link from 'next/link';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'My Blog Site',
   description: 'Short Blogs on various topics',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          <div>
+            <Link href="/">Home</Link>
+          </div>
+        </header>
+        {children}
+        <footer>Di & Co. All rights reserved.</footer>
+      </body>
     </html>
-  )
+  );
 }
